68b57872cc20b15a1420a5f62515d81c5b22ba53
Alissa <alissa.kruiniger@gmail.com>
2025-09-11 16:23:20 +0200

    Quartz sync: Sep 11, 2025, 4:23 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "SP4RROW",
+    pageTitle: "Alissa Maria",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "sp4rrow.net",
+    baseUrl: "alissamaria.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {

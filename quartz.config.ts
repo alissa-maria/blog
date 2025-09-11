e13051862aec1a9458b39127a8ad26478017c730
Alissa <alissa.kruiniger@gmail.com>
2025-09-11 15:23:26 +0200

    Quartz sync: Sep 11, 2025, 3:23 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "SP4RROW",
     pageTitleSuffix: "",
-    enableSPA: false,
+    enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
